7308d4da1e03c4ef786ee5e7f430ffb76ba1d84b
cold-pumpkin <pch4623@gmail.com>
2022-12-25 21:55:49 +0900

    Making Forms Alone

---
+++ pages/forms.tsx
@@ -0,0 +1,62 @@
+import React, { use, useState } from "react";
+
+export default function Forms() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onUsernameChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = event;
+        setUsername(value);
+    };
+
+    const onEmailChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = event;
+        setEmail(value);
+    };
+
+    const onPasswordChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = event;
+        setPassword(value);
+    };
+
+    const onSubmit = (event:React.SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(username, password, email);
+    }
+
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input 
+                value={username}
+                onChange={onUsernameChange}
+                type="text" 
+                placeholder="Username" 
+                required
+                minLength={5}
+            />
+            <input 
+                value={email}
+                onChange={onEmailChange}
+                type="email" 
+                placeholder="Email" 
+                required
+            />
+            <input 
+                value={password}
+                onChange={onPasswordChange}
+                type="password" 
+                placeholder="Password" 
+                required
+            />
+            <input type="submit" value="Create Account" />
+        </form>
+    );
+}
