50d4122fb92fa60a083a95da87913925ba1d9909
cold-pumpkin <pch4623@gmail.com>
2022-12-25 22:51:24 +0900

    Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -8,7 +8,16 @@ interface LoginFrom {
 }
 
 export default function Forms() {
-    const { register, handleSubmit } = useForm<LoginFrom>();
+    const { 
+        register, 
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<LoginFrom>({
+        mode: "onChange"
+    });
+
     const onValid = (data:LoginFrom) => {
         console.log("I'm valid!");
     }
@@ -30,13 +39,20 @@ export default function Forms() {
                 type="text" 
                 placeholder="Username" 
             />
+
             <input 
                 {...register("email", {
                     required: "Email is required!",
+                    validate: {
+                        notGmail: (value) => !value.includes("@gmail.com") || "Gmail is not allowed",
+                    }
                 })}
                 type="email" 
                 placeholder="Email" 
+                className={`${Boolean(errors.email) ? "border-red-700" : ""}`}
             />
+            {errors.email?.message}
+
             <input
                 {...register("password", {
                     required: "Password is required!",
