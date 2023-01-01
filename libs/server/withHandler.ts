841ade1812fa397b468646016e66fa8e7b5fd4e9
cold-pumpkin <pch4623@gmail.com>
2023-01-01 23:05:12 +0900

    withHandler

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function withHandler(
+    method:"GET"|"POST"|"DELETE", 
+    fn: (req: NextApiRequest, res:NextApiResponse) => void
+) {
+    // 로직 실행 후 넘겨받은 함수를 실행
+    return async function(req: NextApiRequest, res:NextApiResponse) {
+        if (req.method !== method) {
+            return res.status(405).end();
+        }
+        try {
+            await fn(req, res)
+        } catch(error)  {
+            console.log(error);
+            return res.status(500).json({ error });
+        }
+    };
+
+}
