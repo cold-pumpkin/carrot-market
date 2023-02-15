978b549ff3bd77ca3eb95df7374e5adbb8b03a34
cold-pumpkin <pch4623@gmail.com>
2023-02-15 23:48:52 +0900

    Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,11 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { prisma } from "@prisma/client";
 
 async function handler(
-    req: NextApiRequest, res:NextApiResponse
+	req: NextApiRequest, res:NextApiResponse
 ) {
-    console.log(req.body);
-    res.status(200).end();
+	const { email, phone } = req.body;
+	const payload = phone ? { phone: +phone } : { email };
+	
+	// upsert
+	const user = await client.user.upsert({
+		where: {
+			...payload,
+		},	
+		create: {
+			name: "Anonymous",
+			...payload,
+		},
+		update: {},
+	});
+	
+	console.log("user : ", user);
+
+	res.status(200).end();
+
 }
 
 export default withHandler("POST", handler);
