69ecaa73ab6e7860f2abf9dcc637a8c7f48b6cca
cold-pumpkin <pch4623@gmail.com>
2023-02-20 23:25:20 +0900

    Token UI

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -51,16 +51,16 @@ async function handler(
 	console.log("token : ", token);
 
 	if (phone) {
-		const message = await twilioClient.messages.create({
+/* 		const message = await twilioClient.messages.create({
 			messagingServiceSid: process.env.TWILIO_MSID,
 			to: process.env.MY_PHONE!,	// 메시지 확인을 위해 body의 phone 대신 내 번호 셋팅
 			body: `Your login token is ${payload}.`
 		});
 
-		console.log("message : ", message);
+		console.log("message : ", message); */
 
 	} else if (email) {
-		const mail = await nodemailer.createTransport({
+		/* const mail = await nodemailer.createTransport({
 			service: 'Gmail',
     	auth: { 
 				user: process.env.MY_EMAIL, 
@@ -74,17 +74,17 @@ async function handler(
 		const mailOptions = {
 			to: process.env.MY_EMAIL,
 			subject: '[carrot market]가입 인증 메일',
-			html: `Your token is <strong>${payload}</strong>`,
+			html: `Your token is <strong>${token}</strong>`,
 		}
 
 		const message = await mail.sendMail(mailOptions);
 		console.log("message: ", message);
-
+		*/
 	}
 	
-
-	res.status(200).end();
-
+	return res.json({
+		ok: true,
+	});
 }
 
 export default withHandler("POST", handler);
