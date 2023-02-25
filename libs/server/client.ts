806390faa76a22136a9805a2cf28f6c12c3acd6f
cold-pumpkin <pch4623@gmail.com>
2023-02-25 13:18:54 +0900

    Product Model

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+  var client: PrismaClient | undefined
+}
+
+const client = global.client || new PrismaClient(); // global.client가 없으면 새로운 클라이언트 생성
+
+if (process.env.NODE_ENV === "development") {
+  global.client = client;
+}
+
+export default client;
