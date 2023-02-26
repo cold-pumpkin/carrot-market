163da3f601659a0b7743ef9d1c6eb62c4e22302f
cold-pumpkin <pch4623@gmail.com>
2023-02-26 11:57:00 +0900

    Favorite Products

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -3,46 +3,40 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
 
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   if (req.method === "GET") {
     const products = await client.product.findMany({});
-
     res.json({
       ok: true,
       products,
     });
   }
-
   if (req.method === "POST") {
-    const { 
-      body : { name, price, description },
-      session : { user },
+    const {
+      body: { name, price, description },
+      session: { user },
     } = req;
-  
     const product = await client.product.create({
       data: {
-        name, 
-        price: +price,   // 숫자 타입으로 변경
+        name,
+        price: +price,
         description,
-        image: "XX",
+        image: "xx",
         user: {
           connect: {
             id: user?.id,
-          }
-        }
+          },
+        },
       },
     });
-  
     res.json({
       ok: true,
       product,
     });
   }
-  
 }
 
 export default withApiSession(withHandler({
