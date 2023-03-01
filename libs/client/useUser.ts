f6100cb25e55f4caa5f79a54c15e1faad0baf061
cold-pumpkin <pch4623@gmail.com>
2023-03-01 17:14:18 +0900

    fix typo

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import userSWR from "swr"; 
+import useSWR from "swr"; 
 
 
 // 로그인 유저 정보를 가져오는 Hook
 export default function userUser() {
-  const { data, error } = userSWR("/api/users/me");  // SWR이 해당 URL에 해당하는 값 캐싱 & 업데이트
+  const { data, error } = useSWR("/api/users/me");  // SWR이 해당 URL에 해당하는 값 캐싱 & 업데이트
   const router = useRouter();
 
   useEffect(() => {
