4950e4d476b65bc03dfd9cd8a2bf3147f06b39bb
cold-pumpkin <pch4623@gmail.com>
2023-03-12 16:55:43 +0900

    CloudFlare Images - Image Preview

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,7 +1,7 @@
 import useMutation from "@libs/client/useMutation";
 import useUser from "@libs/client/useUser";
 import type { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "@components/button";
 import Input from "@components/input";
@@ -11,6 +11,7 @@ interface EditProfileForm {
   email?: string;
   phone?: string;
   name?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -26,21 +27,22 @@ const EditProfile: NextPage = () => {
     setValue,
     handleSubmit,
     setError,
-    formState: { errors }
+    formState: { errors },
+    watch, // 변경감지
   } = useForm<EditProfileForm>();
- 
+
   useEffect(() => {
     if (user?.email) setValue("email", user.email);
     if (user?.phone) setValue("phone", user.phone);
-    if (user?.name)  setValue("name", user.name);
-  }, [user, setValue]);  
+    if (user?.name) setValue("name", user.name);
+  }, [user, setValue]);
 
   const [editProfile, { data, loading }] = useMutation<EditProfileResponse>(`/api/users/me`);
 
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
-    if (email === '' && phone === '' && name === '') { 
-      return setError("formErrors", { message :"이메일 혹은 휴대폰 번호 정보를 입력해주세요!" });
+    if (email === '' && phone === '' && name === '') {
+      return setError("formErrors", { message: "이메일 혹은 휴대폰 번호 정보를 입력해주세요!" });
     }
 
     editProfile({ email, phone, name });
@@ -48,21 +50,38 @@ const EditProfile: NextPage = () => {
 
   useEffect(() => {
     if (data && !data.ok && data.error) {
-      setError("formErrors", {message: data.error});
+      setError("formErrors", { message: data.error });
     }
   }, [data]);
-  
+
+  const [avatarPreview, setAvatarPreview] = useState("");
+  const avatar = watch("avatar");
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      console.log(avatar);
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));  // 브라우저 메모리에 있는 이미지의 URL에 접근
+    }
+  }, [avatar]);
+
   return (
     <Layout canGoBack title="Edit Profile">
       <form onSubmit={handleSubmit(onValid)} className="zpy-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ?
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            /> :
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          }
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
@@ -92,11 +111,11 @@ const EditProfile: NextPage = () => {
           kind="phone"
         />
         {errors.formErrors ? (
-            <span className="my-2 text-red-500 font-medium text-center block">
-              {errors.formErrors.message}
-            </span>
-          ) : null
-          }
+          <span className="my-2 text-red-500 font-medium text-center block">
+            {errors.formErrors.message}
+          </span>
+        ) : null
+        }
         <Button text={loading ? "Loading..." : "Update profile"} />
       </form>
     </Layout>
